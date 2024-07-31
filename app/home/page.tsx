16306bcde31adae3445f4d337b7868e3388453d5
Andre Fong <fongandre123@gmail.com>
2024-07-31 16:27:39 -0400

    feat: Dashboard UI

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,34 +1,123 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./home.module.scss";
 import Button from "@mui/material/Button";
+import PublicRoundedIcon from "@mui/icons-material/PublicRounded";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
 
 export default function Home() {
+  const yourUsername = "mandre";
   const portfolioNames = ["Tech", "Healthcare", "Energy"];
+  const stockListNames = [
+    { name: "Farmers", isPublic: true },
+    { name: "Tech", isPublic: false },
+    { name: "Retail", isPublic: true },
+  ];
+  const sharedStockListNames = [
+    { name: "Auto", owner: "zanesun", isPublic: false },
+    { name: "Retail", owner: "zobiebuttz", isPublic: true },
+    { name: "Finance", owner: "victo", isPublic: false },
+  ];
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Dashboard</h1>
+      <h1 className={styles.title}>Welcome, mandre.</h1>
 
       <div className={styles.section}>
         <div className={styles.row}>
           <h2 className={styles.section_title}>Your Portfolios</h2>
-          <Button variant="contained" href="/new/portfolio">
+          <Button
+            variant="contained"
+            href="/new/portfolio"
+            title="New portfolio"
+          >
             + New
           </Button>
+        </div>
+
+        <div className={styles.list}>
+          {portfolioNames.map((name) => (
+            <Link href={`/portfolios/${name}`} key={name}>
+              <div className={styles.portfolio_card}>
+                <Image
+                  src="/portfolio-asset.png"
+                  width={100}
+                  height={70}
+                  alt="Portfolio asset"
+                />
+                <h3 className={styles.portfolio_name}>{name}</h3>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
 
       <div className={styles.section}>
         <div className={styles.row}>
           <h2 className={styles.section_title}>Your Stock Lists</h2>
-          <Button variant="contained" href="/new/stock-list">
+          <Button
+            variant="contained"
+            href="/new/stock-list"
+            title="New stock list"
+          >
             + New
           </Button>
+        </div>
+
+        <div className={styles.stocklist_list}>
+          {stockListNames.map((stockList) => (
+            <Link
+              href={`/stocklists/${yourUsername}/${stockList.name}`}
+              key={stockList.name}
+            >
+              <div className={styles.stocklist_card}>
+                <Image
+                  src="/stocklist-asset.png"
+                  width={85}
+                  height={80}
+                  alt="Stock list asset"
+                />
+                <div className={styles.stocklist_info}>
+                  {stockList.isPublic && <PublicRoundedIcon fontSize="small" />}
+                  <h3 className={styles.stocklist_name}>{stockList.name}</h3>
+                </div>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
 
       <div className={styles.section}>
         <h2 className={styles.section_title}>Stock Lists Shared With You</h2>
+
+        <div className={styles.stocklist_list}>
+          {sharedStockListNames.map((stockList) => (
+            <Link
+              href={`/stocklists/${stockList.owner}/${stockList.name}`}
+              key={stockList.name}
+            >
+              <div className={styles.stocklist_card}>
+                <div className={styles.stocklist_asset}>
+                  <Image
+                    src="/stocklist-asset.png"
+                    width={85}
+                    height={80}
+                    alt="Stock list asset"
+                  />
+                  <div className={styles.profile_icon}>
+                    <AccountCircleRoundedIcon fontSize="large" />
+                  </div>
+                </div>
+                <div className={styles.stocklist_info}>
+                  {stockList.isPublic && <PublicRoundedIcon fontSize="small" />}
+                  <h3 className={styles.stocklist_name}>{stockList.name}</h3>
+                </div>
+
+                <h4 className={styles.stocklist_author}>({stockList.owner})</h4>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,8 @@ import DashboardRoundedIcon from "@mui/icons-material/DashboardRounded";
 import FolderCopyRoundedIcon from "@mui/icons-material/FolderCopyRounded";
 import SummarizeRoundedIcon from "@mui/icons-material/SummarizeRounded";
 import PeopleAltRoundedIcon from "@mui/icons-material/PeopleAltRounded";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
 import Tooltip from "@mui/material/Tooltip";
 import Link from "next/link";
 
@@ -12,29 +14,48 @@ export default function Dashboard() {
     <div className={styles.content}>
       <Paper elevation={5} sx={{ padding: "35px 20px", borderRadius: "20px" }}>
         <div className={styles.container}>
-          <Tooltip title="Dashboard" placement="right" arrow>
-            <Link href="/home">
-              <DashboardRoundedIcon fontSize="large" />
-            </Link>
-          </Tooltip>
+          <div className={styles.icon_container}>
+            <Tooltip title="Dashboard" placement="right" arrow>
+              <Link href="/home">
+                <DashboardRoundedIcon fontSize="large" />
+              </Link>
+            </Tooltip>
 
-          <Tooltip title="Portfolios" placement="right" arrow>
-            <Link href="/portfolios">
-              <FolderCopyRoundedIcon fontSize="large" />
-            </Link>
-          </Tooltip>
+            <Tooltip title="Portfolios" placement="right" arrow>
+              <Link href="/portfolios">
+                <FolderCopyRoundedIcon fontSize="large" />
+              </Link>
+            </Tooltip>
 
-          <Tooltip title="Stock Lists" placement="right" arrow>
-            <Link href="/stocklists">
-              <SummarizeRoundedIcon fontSize="large" />
-            </Link>
-          </Tooltip>
+            <Tooltip title="Stock Lists" placement="right" arrow>
+              <Link href="/stocklists">
+                <SummarizeRoundedIcon fontSize="large" />
+              </Link>
+            </Tooltip>
 
-          <Tooltip title="Friends" placement="right" arrow>
-            <Link href="/friends">
-              <PeopleAltRoundedIcon fontSize="large" />
-            </Link>
-          </Tooltip>
+            <Tooltip title="Friends" placement="right" arrow>
+              <Link href="/friends">
+                <PeopleAltRoundedIcon fontSize="large" />
+              </Link>
+            </Tooltip>
+          </div>
+
+          <div className={styles.icon_container} style={{ gap: "15px" }}>
+            <Tooltip title="Profile" placement="right" arrow>
+              <Link href="/profile">
+                <AccountCircleRoundedIcon
+                  fontSize="large"
+                  sx={{ color: "black" }}
+                />
+              </Link>
+            </Tooltip>
+
+            <Tooltip title="Logout" placement="right" arrow>
+              <Link href="/logout">
+                <LogoutRoundedIcon fontSize="large" />
+              </Link>
+            </Tooltip>
+          </div>
         </div>
       </Paper>
     </div>
