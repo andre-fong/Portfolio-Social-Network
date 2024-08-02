ec2cb6f4f4153eb115c85e50de517113d10f4232
Andre Fong <fongandre123@gmail.com>
2024-08-01 20:30:02 -0400

    dep: Install @mui/x-date-pickers

--- components/StockMatrix.tsx
+++ components/StockMatrix.tsx
@@ -3,6 +3,9 @@ import styles from "./styles/StockMatrix.module.scss";
 import { useState } from "react";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 export default function StockMatrix() {
   const [matrixType, setMatrixType] = useState("covariance");
@@ -27,6 +30,12 @@ export default function StockMatrix() {
           <ToggleButton value="covariance">Covariance</ToggleButton>
           <ToggleButton value="correlation">Correlation</ToggleButton>
         </ToggleButtonGroup>
+      </div>
+
+      <div className={styles.date_fields}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker label="Start Date" />
+        </LocalizationProvider>
       </div>
     </div>
   );
