29e5f9661f18b94e820c4dff9e218a28400c30b9
Andre Fong <fongandre123@gmail.com>
2024-08-03 08:41:35 -0400

    feat: Stock page functionality

--- components/NewStockData.tsx
+++ components/NewStockData.tsx
@@ -16,6 +16,87 @@ export default function NewStockData() {
   const [modalOpen, setModalOpen] = useState(false);
   const [newStockChecked, setNewStockChecked] = useState(false);
 
+  function handleAddData() {
+    console.log(
+      new Date(
+        (
+          document.querySelector(
+            "input[placeholder='MM/DD/YYYY']"
+          ) as HTMLInputElement
+        ).value
+      )
+    );
+
+    if (newStockChecked) {
+      // Add new stock
+      fetch("/api/stock/createStock", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          credentials: "include",
+        },
+        body: JSON.stringify({
+          tickerSymbol: (document.getElementById("symbol") as HTMLInputElement)
+            .value,
+        }),
+      });
+
+      // Add data to new stock
+      setTimeout(() => {
+        fetch("/api/stock/addStockData", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            credentials: "include",
+          },
+          body: JSON.stringify({
+            ticker_symbol: (
+              document.getElementById("symbol") as HTMLInputElement
+            ).value,
+            timestamp: new Date(
+              (
+                document.querySelector(
+                  "input[placeholder='MM/DD/YYYY']"
+                ) as HTMLInputElement
+              ).value
+            ),
+            open: (document.getElementById("open") as HTMLInputElement).value,
+            high: (document.getElementById("high") as HTMLInputElement).value,
+            low: (document.getElementById("low") as HTMLInputElement).value,
+            close: (document.getElementById("close") as HTMLInputElement).value,
+            volume: (document.getElementById("volume") as HTMLInputElement)
+              .value,
+          }),
+        });
+      }, 300);
+    } else {
+      // Add data to existing stock
+      fetch("/api/stock/addStockData", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          credentials: "include",
+        },
+        body: JSON.stringify({
+          ticker_symbol: (document.getElementById("symbol") as HTMLInputElement)
+            .value,
+          timestamp: new Date(
+            (
+              document.querySelector(
+                "input[placeholder='MM/DD/YYYY']"
+              ) as HTMLInputElement
+            ).value
+          ),
+          open: (document.getElementById("open") as HTMLInputElement).value,
+          high: (document.getElementById("high") as HTMLInputElement).value,
+          low: (document.getElementById("low") as HTMLInputElement).value,
+          close: (document.getElementById("close") as HTMLInputElement).value,
+          volume: (document.getElementById("volume") as HTMLInputElement).value,
+        }),
+      });
+    }
+  }
+
   return (
     <>
       <Button variant="contained" onClick={() => setModalOpen(true)}>
@@ -38,6 +119,7 @@ export default function NewStockData() {
               variant="outlined"
               margin="dense"
               autoComplete="off"
+              id="symbol"
             />
 
             {/* Checkbox that says this is a new stock */}
@@ -66,6 +148,7 @@ export default function NewStockData() {
               variant="outlined"
               margin="dense"
               autoComplete="off"
+              id="open"
             />
             <TextField
               fullWidth
@@ -73,6 +156,7 @@ export default function NewStockData() {
               variant="outlined"
               margin="dense"
               autoComplete="off"
+              id="high"
             />
             <TextField
               fullWidth
@@ -80,6 +164,7 @@ export default function NewStockData() {
               variant="outlined"
               margin="dense"
               autoComplete="off"
+              id="low"
             />
             <TextField
               fullWidth
@@ -87,6 +172,7 @@ export default function NewStockData() {
               variant="outlined"
               margin="dense"
               autoComplete="off"
+              id="close"
             />
             <TextField
               fullWidth
@@ -94,8 +180,13 @@ export default function NewStockData() {
               variant="outlined"
               margin="dense"
               autoComplete="off"
+              id="volume"
             />
-            <Button variant="contained" sx={{ marginTop: "25px" }}>
+            <Button
+              variant="contained"
+              sx={{ marginTop: "25px" }}
+              onClick={handleAddData}
+            >
               Add Data
             </Button>
           </div>
